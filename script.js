fc2b2a2c43fc61f3ae8f17a6cf92196e4f3a2662
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-05-02 18:22:01 +0200

    Add getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello, World");
+function getComputerChoice() {
+  let choiceNumber = Math.floor(Math.random() * 3);
+
+  let choiceString;
+
+  switch (choiceNumber) {
+    case 0:
+      choiceString = "rock";
+      break;
+    case 1:
+      choiceString = "paper";
+      break;
+    case 2:
+      choiceString = "scissors";
+      break;
+  }
+
+  return choiceString;
+}
